6f2e6fa9e327cc011651852f69a311653a60a6e7
Kevin Alexander Arevalo <Kaas7520@gmail.com>
2023-03-11 21:52:18 -0500

    Se creo modelos => juegos y requisitos

---
+++ src/app/Models/juegos.model.ts
@@ -0,0 +1,13 @@
+import { RequisitosModel } from "./requisitos.model";
+
+export interface JuegosModel {
+    id              : number,
+    requisitos      : RequisitosModel,
+    src             : string,
+    nombre          : string,
+    descripcion     : string,
+    precio          : number,
+    fechaLazamiento : string,
+    descuento       : number,
+    estado          : string,
+}
---
+++ src/app/Models/requisitos.model.ts
@@ -0,0 +1,11 @@
+export interface RequisitosModel {
+    id             : number;
+    sistema        : string,
+    procesador     : string,
+    memoria        : string,
+    graficos       : string,
+    directx        : string,
+    almacenamiento : string,
+    tipoRequisitos : string,
+    estado         : string
+}
