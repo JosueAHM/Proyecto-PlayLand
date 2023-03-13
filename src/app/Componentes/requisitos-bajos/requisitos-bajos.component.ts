a44791a9994c86b1f3aa11aefe449ecf3939914e
Kevin Alexander Arevalo <Kaas7520@gmail.com>
2023-03-12 19:03:11 -0500

    Se corrigio pantalla requisitos bajos y se agrego apis

--- src/app/Componentes/requisitos-bajos/requisitos-bajos.component.ts
+++ src/app/Componentes/requisitos-bajos/requisitos-bajos.component.ts
@@ -2,72 +2,68 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Requisitos } from 'src/app/interface/juegos';
 import { CompraComponent } from '../compra/compra.component';
+import { JuegosService_BE } from 'src/app/services/juegos_be.service';
+import { GeneralJuegos } from 'src/app/Models/consulta_general.model';
+import { RequisitosModel } from 'src/app/Models/requisitos.model';
+import { JuegosModel } from 'src/app/Models/juegos.model';
 
 @Component({
   selector: 'app-requisitos-bajos',
   templateUrl: './requisitos-bajos.component.html',
   styleUrls: ['./requisitos-bajos.component.css']
 })
-export class RequisitosBajosComponent {
+export class RequisitosBajosComponent implements OnInit {
 
-  ArrayRequisitos: string[] = ['sistema', 'procesador', 'memoria', 'graficos', 'directx', 'almacenamiento'];
+  constructor(
+    private dialog: MatDialog,
+    private _serviceJuego_BE: JuegosService_BE
+  ) {}
 
-  ArrayObjRequisitos: Requisitos[] = [
-    {
-      sistema: 'Windows Vista or better: 64 bits required',
-      procesador: 'Intel 2.0ghz Core 2 Duo or equivalent',
-      memoria: '1 GB de RAM',
-      graficos: 'Nvidia 450 GTS / Radeon HD 5750 or better',
-      directx: 'Versión 10',
-      almacenamiento: ' 1 GB de espacio disponible'
-    },
-    {
-      sistema: ' Windows 7',
-      procesador: 'X64 Quad Core CPU 2.0GHz+',
-      memoria: '8 GB de RAM',
-      graficos: '2GB VRAM',
-      directx: ' Versión 11',
-      almacenamiento: ' 4 GB de espacio disponible'
-    },
-    {
-      sistema: 'Windows 7',
-      procesador: 'Core 2 Duo',
-      memoria: '4 GB de RAM',
-      graficos: 'Intel HD Graphics 4000 or other shader model 4.0',
-      directx: 'Versión 10',
-      almacenamiento: ' 1 GB de espacio disponible'
-    },
-    {
-      sistema: 'Windows® 8.1 64 bit',
-      procesador: 'Intel® Core™ i3-2105 / AMD® FX 4300',
-      memoria: '4 GB de RAM',
-      graficos: ' Nvidia® GeForce™ GTX 460 / AMD® Radeon™ HD 5850',
-      directx: 'Versión 9.0c',
-      almacenamiento: ' 6 GB de espacio disponible'
-    },
-    {
-      sistema: ' Windows® 7 64 Bit | Windows® 8.1 64 Bit | Windows® 10 Home 64 Bit',
-      procesador: 'Intel® Core™ i5 750 | AMD® FX 4300',
-      memoria: '4 GB de RAM',
-      graficos: 'Nvidia® GeForce™ GTX 470 (1.28GB) | AMD® HD 5850 (1GB) | Intel Iris Xe G7 (Tiger Lake) | AMD® Radeon™ RX Vega 11 | Steam Deck',
-      directx: 'Versión 9.0c',
-      almacenamiento: ' 2 GB de espacio en disco duro'
-    },
-    {
-      sistema: 'Windows 7/8/10',
-      procesador: '  Intel Core 2 Duo E6320 or equivalent',
-      memoria: ' 2 GB de RAM',
-      graficos: 'GeForce 7600 512 Mb or equivalent',
-      directx: 'Versión 10',
-      almacenamiento: '500 MB de espacio disponible'
-    },
-  ]
-
-  constructor(private dialog: MatDialog) {
-
+  ngOnInit(): void {
+    this.obtenerJuego();
   }
 
-  openDialogSesion() {
-    this.dialog.open(CompraComponent)
+  juegosDisponibles : any[] = [];
+
+  obtenerJuego(){
+    let temporalConsulta : GeneralJuegos = {
+      variableEntrada : "BAJOS",
+      transaccion     : "CONSULTA_JUEGOS_REQUISITOS"
+    }
+    this._serviceJuego_BE.getJuegosLazamientos(temporalConsulta).subscribe((juegos:any)=>{
+      if(juegos.Table1[0].respuesta =! "OK 200"){
+        alert("Error al cargar los juegos => "+juegos.Table1[0].leyenda);
+      }
+      else{
+        juegos.Table.forEach((juego:any) => {
+
+          let requisitosTemporal : RequisitosModel = {
+            sistema: juego.sistema,
+            procesador: juego.procesador,
+            memoria: juego.memoria,
+            graficos: juego.graficos,
+            directx: juego.directx,
+            almacenamiento: juego.almacenamiento,
+            tipoRequisitos: juego.tipoRequisitos,
+            id: null,
+            estado: null
+          }
+
+          let juegoTemporal : JuegosModel = {
+            id : juego.id,
+            requisitos : requisitosTemporal,
+            src : juego.src,
+            nombre : juego.nombre,
+            descripcion : juego.descripcion,
+            precio : juego.precio,
+            fechaLazamiento : juego.fechaLazamiento,
+            descuento : juego.descuento,
+            estado : juego.estado,
+          }
+          this.juegosDisponibles.push(juegoTemporal);
+        });
+       console.log(this.juegosDisponibles);
+      }
+    });
   }
 }
